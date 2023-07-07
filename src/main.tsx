fa17ddd73895726ba60c8f54c099913ca0359179
Daulet Tulepbergenov <daulet6368@gmail.com>
2023-07-08 01:20:39 +0600

    chore: add linters, alias

--- src/main.tsx
+++ src/main.tsx
@@ -5,4 +5,5 @@ import "@fontsource/roboto/700.css";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
